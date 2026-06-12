13651702aaecd7eaeec04363cae4c0701120ef7b
ishaharish <ishaharish11@gmail.com>
2026-06-12 14:34:49 +0530

    Add deployment scripts and configure project

--- Program.cs
+++ Program.cs
@@ -60,6 +60,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+// Automatically apply pending EF Core migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
